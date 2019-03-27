824ba8698d30682300d4c2fd2d9d7280dd521f47
Haim Taragano <haimt@zoara.com>
2019-03-27 14:06:31 +0200

    deploy test

--- start/routes.js
+++ start/routes.js
@@ -16,21 +16,24 @@
 const Route = use('Route')
 
 const Drive = use('Drive')
+
+const Helpers = use('Helpers')
+
 Route.group(() => {
   Route.get('/global', () => {
-    return Drive.get('../resources/cached-responses/global.json')
+    return Drive.get(Helpers.appRoot('storage/cached-responses/global.json'))
   })
   Route.get('/main/index.json', () => {
-    return Drive.get('../resources/cached-responses/main-index.json')
+    return Drive.get(Helpers.appRoot('storage/cached-responses/main-index.json'))
   })
   Route.get('/dyo/diamond/:filter?', () => {
-    return Drive.get('../resources/cached-responses/dyo-diamond.json')
+    return Drive.get(Helpers.appRoot('storage/cached-responses/dyo-diamond.json'))
   })
   Route.get('/diamonds', () => {
-    return Drive.get('../resources/cached-responses/dyo-diamond.json')
+    return Drive.get(Helpers.appRoot('storage/cached-responses/dyo-diamond.json'))
   })
   Route.get('/dyo/color_diamond/:filter?', () => {
-    return Drive.get('../resources/cached-responses/dyo-color_diamond.json')
+    return Drive.get(Helpers.appRoot('storage/cached-responses/dyo-color_diamond.json'))
   })
 }
 ).prefix('api')
